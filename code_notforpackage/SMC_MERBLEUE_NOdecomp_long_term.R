4a61a5b15b3d8f49318743b4f9a03558deceec32
jeroenpullens <jeroenpullens@gmail.com>
2015-12-07 11:32:33 +0100

    updated proposal function

--- code_notforpackage/SMC_MERBLEUE_NOdecomp_long_term.R
+++ code_notforpackage/SMC_MERBLEUE_NOdecomp_long_term.R
@@ -2,7 +2,7 @@ library(NUCOMBog)
 # library(BayesianTools)
 
 
-setup_SMC<-setup(mainDir="/home/jeroen/MERBLEUE_long_term/",climate="Dataset_1939-2013.txt",environment="Env_Mer_Bleue.txt",inival="Inival_Mer_Bleue.txt",start=1939,end=2013,type=c("NEE","WTD"),numFolders=30000,separate = F,startval=721)
+setup_SMC<-setup(mainDir="/home/jeroen/MERBLEUE_long_term/",climate="Dataset_1939-2013.txt",environment="Env_Mer_Bleue.txt",inival="Inival_Mer_Bleue.txt",start=1939,end=2013,type=c("NEE","WTD"),numFolders=10,separate = F,startval=721)
 setwd(setup_SMC$runParameters[[1]]$mainDir)
 data<-read.csv("input/NEE_WTD_GPP_MERBLEUE_1999_2013.csv",sep="\t",as.is=T)
 data<-data[2:nrow(data),]
@@ -14,21 +14,20 @@ values<-c(0.5,70,0.08,0.012,0.8,60,0.08,0.012,100,1,45,0.04,20,14,17,1,50,0.04,2
 min<-   0.1*values
 max<-  c(2,5,5,2,1.25,5,5,2,5,5,5,5,3,4.29,3.53,5,5,5,3,4.29,3.53,5,5,5,3,6,3.53,5,5,5)*values
 
-
 par<-matrix(runif(length(setup_SMC$runParameters)*length(names),min=min,max=max),nrow = length(names))
-
 
 parind<-data.frame(names,par)
 names(parind)<-c("names",rep("values",ncol(par)))
 parind$names<-as.character(parind$names)
 
 priortest2<-function(x) {
-  sum(dunif(x,min=(min*values),max=max(max*values),log=T))
+  sum(dunif(x,min=(min),max=max(max),log=T))
 }
-# proposal<-function(x){
-#   rnorm(length(x),mean=x,sd=values*0.1)
-# }
 
-test_smc_nodecomp_long_term<-smc_sampler_mod(likelihood = likelihoodParallel,prior =priortest2,clustertype = "SOCK",numCores = 1,initialParticles = par,setup=setup_SMC, iterations =25, resampling = T, proposal = NULL, parallel="external",scaled=F,Logtype="corrected")
+proposal<-function(x){
+   rnorm(length(x),mean=x,sd=values*0.2)
+ }
+
+test_smc_nodecomp_long_term<-smc_sampler_mod(likelihood = likelihoodParallel,prior =priortest2,clustertype = "SOCK",numCores = 1,initialParticles = par,setup=setup_SMC, iterations =5, resampling = T, proposal = proposal, parallel="external",scaled=F,Logtype="corrected")
 
 save(test_smc_nodecomp_long_term,file="test_smc_nodecomp_longterm_corLL_NewPrior_30112015.rData")
