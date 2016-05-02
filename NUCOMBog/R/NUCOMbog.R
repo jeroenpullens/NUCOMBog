de232e9f57de2e66ec0cd665c44f310494bc99e2
jeroenpullens <johannes.pullens@fmach.it>
2016-05-02 13:49:52 +0200

    Create NUCOMbog.R

---
+++ NUCOMBog/R/NUCOMbog.R
@@ -0,0 +1,13 @@
+#'@title NUCOMBog
+#'
+#'@author JWM Pullens
+#'@source The model can be sent upon request at jeroenpullens[at]gmail[dot]com
+#'@docType package
+#'@description The NUCOMBog R package provides an interface to the NUCOMBog model in R.
+#'
+#'The package simulates the dynamics of five plant functional types (PFTs): graminoids, ericaceous shrubs and three groups of \emph{Sphagnum} mosses (lawn, hollow and hummock mosses) on a monthly time step. The R package also calculates the monthly heterotrophic respiration and therefore the net ecosystem exchange can be calculated.
+#'The package provides a user-friendly tool that allows simulating peatlands over years/decades, under different management strategies and climate change scenarios.
+#'
+#'
+#'For details on how to use the packgage, go to the help files of the functions
+NULL
