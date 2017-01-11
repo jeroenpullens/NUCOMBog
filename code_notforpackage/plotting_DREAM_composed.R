4d492f88b4943e2a676c74231626360e1a864802
jeroenpullens <johannes.pullens@fmach.it>
2017-01-11 10:06:34 +0100

    plotting

--- code_notforpackage/plotting_DREAM_composed.R
+++ code_notforpackage/plotting_DREAM_composed.R
@@ -6,10 +6,17 @@ mapvalues<- map$parametersMAP
 parameters<-data.frame(names,mapvalues)
 names(parameters)<-c("names","values")
 
+setup_SMC[[1]]$type <-c("NEE","WTD","hetero_resp","NPP")
+
 out<-runNUCOM(setup = setup_SMC,parameters = parameters)
 
+pdf("NEE.pdf")
 plotTimeSeries(observed = data$NEE,predicted = out$NEE)
+dev.off()
+
+pdf("WTD.pdf")
 plotTimeSeries(observed = data$WTD/100,predicted = out$WTD)
+dev.off()
 
 
 codaObject = getSample(out2, coda = F,thin=500)
@@ -22,6 +29,7 @@ for(i in 1:nrow(codaObject)){
   outcome[[i]]<-runNUCOM(setup = setup_SMC, parameters = newparameters)
 }
 
+pdf("NEE_all.pdf")
 for(i in 1:length(outcome)){
   plot(outcome[[i]]$NEE,ylim=c(-60,25),col="lightgray",type="l")
   par(new=T)
@@ -30,14 +38,26 @@ points(data$NEE,col=2,type="p")
 par(new=T)
 plot(out$NEE,ylim=c(-60,25),col="black",type="l",main=length(outcome))
 par(new=F)
+dev.off()
 
-
+pdf("WTD_all.pdf")
 for(i in 1:length(outcome)){
-  plot(outcome[[i]]$WTD,ylim=c(-0.3,0.1),col="lightgray",type="l")
+  plot(outcome[[i]]$WTD,ylim=c(-0.8,0.1),col="lightgray",type="l")
   par(new=T)
 }
 points(data$WTD/100,col=2,type="p")
 par(new=T)
-plot(out$WTD,ylim=c(-0.3,0.1),col="black",type="l",main=length(outcome))
+plot(out$WTD,ylim=c(-0.8,0.1),col="black",type="l",main=length(outcome))
 par(new=F)
+dev.off()
+
+plot(out$hetero_resp,type="l")
+plot(out$NPP,type="l")
+
+pdf("hetero_NPP.pdf")
+plot(out$hetero_resp,type="l",ylim=c(0,90))
+par(new=T)
+plot(out$NPP,type="l",ylim=c(0,90),col=2)
+par(new=F)
+dev.off()
 
