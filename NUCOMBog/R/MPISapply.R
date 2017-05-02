d164c7d0e65a6b0bb7089cad63d2206a7a2e8747
jeroenpullens <jeroenpullens@gmail.com>
2017-05-02 23:48:37 +0200

    removed any connection with Rmpi

--- NUCOMBog/R/MPISapply.R
+++
@@ -1,9 +0,0 @@
-MPISapply <- function(numcores, runParameters) {
-  rank <- Rmpi::mpi.comm.rank()
-  # master doesnt work the data
-  if (rank > 0){
-    mywork <- runParameters[seq(rank, length(runParameters), numcores)]
-    result <- sapply(mywork, runnucom_wrapper)
-    return(result)
-  }
-}
